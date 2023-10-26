fd9ed6d11ed7087d4306504c15c814407bb57ae1
DIKIWJY <144869228+DIKIWJY@users.noreply.github.com>
2023-10-26 23:33:56 +0700

    Add files via upload

---
+++ Tugas1PHP/latihan1c.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+    <style>
+        table {
+            width: 10%;
+            border-collapse: separate;
+            border-spacing: 20px
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th, td {
+       padding: 15px;
+            text-align: center;
+        }
+
+        td {
+            font-size: 24px; /* Ubah ukuran sesuai kebutuhan Anda */
+        }
+    </style>
+</head>
+<body>
+   <table border="1" cellpading="20" cellspacing="9">
+   <tr>
+    <td>A</td>
+   </tr>
+  <tr>
+    <td>A</td>
+    <td>B</td>
+  </tr>
+  <tr>
+    <td>A</td>
+    <td>B</td>
+    <td>C</td>
+  </tr>
+</table>
+</body>
+</html>
